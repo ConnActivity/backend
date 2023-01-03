3dd97ce90e4946c6417f72dabbe4d7a3554b6341
Bene <37740907+Nereuxofficial@users.noreply.github.com>
2023-01-03 15:09:21 +0100

    Fixed Pagination now finally

--- start/views.py
+++ start/views.py
@@ -88,7 +88,7 @@ def event_list(request, page=1, order="-date_published", format=None, ):
         event = Event.objects.all().order_by(order)
         serializer = PrivateEventSerializer(event, many=True)
         paginator = Paginator(serializer.data, 10)
-        return Response(PrivateEventSerializer(paginator.page(page)))
+        return Response(PrivateEventSerializer(paginator.page(page).object_list, many=True).data)
 
     elif request.method == 'POST':
         serializer = EventSerializer(data=request.data)
